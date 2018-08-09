00442344a962b65320ab4dc11ebfc56118983aa6
Sara Helba <29603156+sjhelba@users.noreply.github.com>
2018-08-09 12:57:00 -0400

    updated templates to include redraw widget checks and to make browser more similar to niagara version

---
+++ TEMPLATES/niagaraTemplate.js
@@ -0,0 +1,232 @@
+define(['bajaux/Widget', 'bajaux/mixin/subscriberMixIn', 'nmodule/COREx/rc/d3/d3.min'], function (Widget, subscriberMixIn, d3) {
+	"use strict";
+
+////////// Hard Coded Defs //////////
+
+	const getJSDateFromTimestamp = d3.timeParse('%d-%b-%y %I:%M:%S.%L %p UTC%Z');
+	const formatIntoPercentage = d3.format('.0%');
+	const getTextWidth = (text, font) => {
+		const canvas = document.createElement('canvas');
+		const context = canvas.getContext('2d');
+		context.font = font;
+		const width = context.measureText(text).width;
+		d3.select(canvas).remove()
+		return width;
+	};
+	const getTextHeight = font => {
+		let num = '';
+		const indexOfLastDigit = font.indexOf('pt') - 1;
+		for(let i = 0; i <= indexOfLastDigit; i++){
+			if(!isNaN(font[i]) || font[i] === '.') num += font[i];
+		}
+		num = +num;
+		return num * 1.33333333333;
+	};
+	const resetElements = (outerWidgetEl, elementsToReset) => {
+		const selectionForCheck = outerWidgetEl.selectAll(elementsToReset)
+		if (!selectionForCheck.empty()) selectionForCheck.remove();
+	};
+	const arePrimitiveValsInObjsSame = (obj1, obj2) => !Object.keys(obj1).some(key => (obj1[key] === null || (typeof obj1[key] !== 'object' && typeof obj1[key] !== 'function')) && obj1[key] !== obj2[key])
+	// 0 layers means obj only has primitive values
+	// this func only works with obj literals layered with obj literals until base layer only primitive
+	const checkNestedObjectsEquivalence = (objA, objB, layers) => {
+		if (layers === 0) {
+			return arePrimitiveValsInObjsSame(objA, objB);
+		} else {
+			const objAKeys = Object.keys(objA);
+			const objBKeys = Object.keys(objB);
+			if (objAKeys.length !== objBKeys.length) return false;
+			const somethingIsNotEquivalent = objAKeys.some(key => {
+				return !checkNestedObjectsEquivalence(objA[key], objB[key], layers - 1);
+			})
+			return !somethingIsNotEquivalent;
+		}
+	};
+	const needToRedrawWidget = (widget, newData) => {
+		const lastData = widget.data;
+		// check primitives for equivalence
+		if (!arePrimitiveValsInObjsSame(lastData, newData)) return true;
+		// check nested objs for equivalence
+		const monthlyModulesAreSame = checkNestedObjectsEquivalence(lastData.monthlyModulesData, newData.monthlyModulesData, 3);
+		const monthlyOverallAreSame = checkNestedObjectsEquivalence(lastData.monthlyOverallData, newData.monthlyOverallData, 2);
+		const annualModulesAreSame = checkNestedObjectsEquivalence(lastData.annualModulesData, newData.annualModulesData, 2);
+		const annualOverallAreSame = checkNestedObjectsEquivalence(lastData.annualOverallData, newData.annualOverallData, 1);
+		if (!monthlyModulesAreSame || !monthlyOverallAreSame || !annualModulesAreSame || !annualOverallAreSame) return true;
+
+		//return false if nothing prompted true
+		return false;
+	};
+
+
+////////////////////////////////////////////////////////////////
+	// Define Widget Constructor & Exposed Properties
+////////////////////////////////////////////////////////////////
+
+	var MyWidget = function () {
+		var that = this;
+		Widget.apply(this, arguments);
+
+		that.properties().addAll([
+			{
+				name: 'backgroundColor',
+				value: 'white',
+				typeSpec: 'gx:Color'
+			},
+			{
+				name: 'includeCTFs',
+				value: true
+			},
+			{
+				name: 'paddingUnderLegendText',
+				value: 5
+			},
+			{
+				name: 'systemName',
+				value: 'systemName'
+			},
+			{
+				name: 'tooltipFillColor',
+				value: '#f2f2f2',
+				typeSpec: 'gx:Color'
+			},
+			{
+				name: 'modulePercentFont',
+				value: 'bold 26.0pt Nirmala UI',
+				typeSpec: 'gx:Font'
+			}
+		]);
+
+
+
+		subscriberMixIn(that);
+	};
+
+	MyWidget.prototype = Object.create(Widget.prototype);
+	MyWidget.prototype.constructor = MyWidget;
+
+
+
+////////////////////////////////////////////////////////////////
+	// /* SETUP DEFINITIONS AND DATA */
+////////////////////////////////////////////////////////////////
+
+
+	const setupDefinitions = widget => {
+		// FROM USER // 
+		const data = widget.properties().toValueMap();	//obj with all exposed properties as key/value pairs
+
+		// FROM JQ //
+		const jq = widget.jq();
+		data.graphicWidth = jq.width() || 350;
+		data.graphicHeight = jq.height() || 400;
+
+
+		// GLOBALS PER INSTANCE
+		if (!widget.hovered) widget.hovered = { optimized: false, standard: false, current: 'neither' };
+		if (!widget.activeModule) widget.activeModule = 'none';
+		if (!widget.percentIsHovered) widget.percentIsHovered = false;
+
+
+		// GET DATA
+		return widget.resolve(`station:|slot:/tekWorxCEO/${data.systemName}`)	
+			.then(system => system.getNavChildren())	// get children folders of system folder
+			.then(folders => {
+				// calculated without ords
+        
+        
+
+
+
+				// calculated with ords
+        
+        
+
+
+
+				return data;
+			})
+			.catch(err => console.error('Error (ord info promise rejected): ' + err));
+	};
+
+
+
+
+////////////////////////////////////////////////////////////////
+	// Render Widget (invoke setupDefinitions() and, using returned data, append D3 elements into SVG)
+////////////////////////////////////////////////////////////////
+
+	const renderWidget = (widget, data) => {
+    d3.select(widget.svg.node().parentNode).style('background-color', data.backgroundColor);
+		// delete leftover elements from versions previously rendered
+		if (!widget.svg.empty()) resetElements(widget.svg, '*');
+		const graphicGroup = widget.svg.append('g').attr('class', 'graphicGroup');
+
+
+
+
+    
+    
+
+
+
+
+
+	};
+
+
+	function render(widget, force) {
+		// invoking setupDefinitions, then returning value from successful promise to renderWidget func
+		return setupDefinitions(widget)
+			.then(data => {
+				if (force || !widget.data || needToRedrawWidget(widget, data)){
+					
+					renderWidget(widget, data);	
+				}
+				widget.data = data;
+			})
+			.catch(err => console.error('render did not run properly: ' + err));
+	}
+
+
+////////////////////////////////////////////////////////////////
+	// Initialize Widget
+////////////////////////////////////////////////////////////////
+
+	MyWidget.prototype.doInitialize = function (element) {
+		var that = this;
+		element.addClass("MyWidgetOuter");
+		const outerEl = d3.select(element[0])
+			.style('overflow', 'hidden')
+
+		that.svg = outerEl.append('svg')
+			.attr('class', 'MyWidget')
+			.style('overflow', 'hidden')
+			.attr('top', 0)
+			.attr('left', 0)
+			.attr('width', "100%")
+			.attr('height', "100%");
+
+		that.getSubscriber().attach("changed", function (prop, cx) { render(that) });
+	};
+
+
+////////////////////////////////////////////////////////////////
+	// Extra Widget Methods
+////////////////////////////////////////////////////////////////
+
+	MyWidget.prototype.doLayout = MyWidget.prototype.doChanged = MyWidget.prototype.doLoad = function () { render(this); };
+
+	/* FOR FUTURE NOTE: 
+	MyWidget.prototype.doChanged = function (name, value) {
+		  if(name === "value") valueChanged += 'prototypeMethod - ';
+		  render(this);
+	};
+	*/
+
+	MyWidget.prototype.doDestroy = function () {
+		this.jq().removeClass("MyWidgetOuter");
+	};
+
+	return MyWidget;
+});
+
---
+++ chillerReport/browser_chillerReport.js
@@ -0,0 +1,246 @@
+function defineFuncForTabSpacing () {
+
+	////////// Hard Coded Defs //////////
+	const getJSDateFromTimestamp = d3.timeParse('%d-%b-%y %I:%M:%S.%L %p UTC%Z');
+	const formatIntoPercentage = d3.format('.0%');
+	const getTextWidth = (text, font) => {
+		const canvas = document.createElement('canvas');
+		const context = canvas.getContext('2d');
+		context.font = font;
+		const width = context.measureText(text).width;
+		d3.select(canvas).remove()
+		return width;
+	};
+	const getTextHeight = font => {
+		let num = '';
+		const indexOfLastDigit = font.indexOf('pt') - 1;
+		for(let i = 0; i <= indexOfLastDigit; i++){
+			if(!isNaN(font[i]) || font[i] === '.') num += font[i];
+		}
+		num = +num;
+		return num * 1.33333333333;
+	};
+	const resetElements = (outerWidgetEl, elementsToReset) => {
+		const selectionForCheck = outerWidgetEl.selectAll(elementsToReset)
+		if (!selectionForCheck.empty()) selectionForCheck.remove();
+	};
+	const arePrimitiveValsInObjsSame = (obj1, obj2) => !Object.keys(obj1).some(key => (obj1[key] === null || (typeof obj1[key] !== 'object' && typeof obj1[key] !== 'function')) && obj1[key] !== obj2[key])
+	// 0 layers means obj only has primitive values
+	// this func only works with obj literals layered with obj literals until base layer only primitive
+	const checkNestedObjectsEquivalence = (objA, objB, layers) => {
+		if (layers === 0) {
+			return arePrimitiveValsInObjsSame(objA, objB);
+		} else {
+			const objAKeys = Object.keys(objA);
+			const objBKeys = Object.keys(objB);
+			if (objAKeys.length !== objBKeys.length) return false;
+			const somethingIsNotEquivalent = objAKeys.some(key => {
+				return !checkNestedObjectsEquivalence(objA[key], objB[key], layers - 1);
+			})
+			return !somethingIsNotEquivalent;
+		}
+	};
+	const needToRedrawWidget = (widget, newData) => {
+		const lastData = widget.data;
+		// check primitives for equivalence
+		if (!arePrimitiveValsInObjsSame(lastData, newData)) return true;
+		// check nested objs for equivalence
+		const monthlyModulesAreSame = checkNestedObjectsEquivalence(lastData.monthlyModulesData, newData.monthlyModulesData, 3);
+		const monthlyOverallAreSame = checkNestedObjectsEquivalence(lastData.monthlyOverallData, newData.monthlyOverallData, 2);
+		const annualModulesAreSame = checkNestedObjectsEquivalence(lastData.annualModulesData, newData.annualModulesData, 2);
+		const annualOverallAreSame = checkNestedObjectsEquivalence(lastData.annualOverallData, newData.annualOverallData, 1);
+		if (!monthlyModulesAreSame || !monthlyOverallAreSame || !annualModulesAreSame || !annualOverallAreSame) return true;
+
+		//return false if nothing prompted true
+		return false;
+	};
+
+
+
+	////////////////////////////////////////////////////////////////
+		// Define Widget Constructor & Exposed Properties
+	////////////////////////////////////////////////////////////////
+	const properties = [
+		/* COLORS */
+		//fills
+		{
+			name: 'backgroundColor',
+			value: 'white',
+			typeSpec: 'gx:Color'
+		},
+		//strokes
+		{
+			name: 'gridStrokeColor',
+			value: 'grey',
+			typeSpec: 'gx:Color'
+		},
+		//text
+		{
+			name: 'xAxisTicksTextColor',
+			value: 'black',
+			typeSpec: 'gx:Color'
+		},
+		/* FONT */
+		{
+			name: 'xAxisTicksTextFont',
+			value: 'bold 8.0pt Nirmala UI',
+			typeSpec: 'gx:Font'
+		},
+	/* PADDING */
+		{
+			name: 'paddingBetweenTHINGS',
+			value: 5
+		},
+	/* OTHER */
+		{
+			name: 'overrideDefaultPrecisionWFacets',
+			value: false
+		},
+		{
+			name: 'minDegreesCategory',
+			value: 30
+		},
+	];
+
+
+	////////////////////////////////////////////////////////////////
+		// /* SETUP DEFINITIONS AND DATA */
+	////////////////////////////////////////////////////////////////
+	const setupDefinitions = widget => {
+		// FROM USER // 
+		const data = {};
+		properties.forEach(prop => data[prop.name] = prop.value);
+
+		// FROM JQ //
+		data.jqHeight = 150;
+		data.jqWidth = 150;
+
+		// SIZING //
+		data.margin = {top: 5, left: 5, right: 5, bottom: 5};
+		data.graphicHeight = data.jqHeight - (data.margin.top + data.margin.bottom);
+		data.graphicWidth = data.jqWidth - (data.margin.left + data.margin.right);
+
+		// GLOBALS PER INSTANCE //
+		if (!widget.hovered) widget.hovered = { optimized: false, standard: false, current: 'neither' };
+		if (!widget.activeModule) widget.activeModule = 'none';
+		if (!widget.percentIsHovered) widget.percentIsHovered = false;
+
+		// DATA TO POPULATE //
+		data.fakeData = [];
+
+		// FAKE DATA //
+		const populateFakeData = () => {
+			data.fakeData.unshift('datum1');
+
+
+
+
+		};
+
+
+
+		// CALCULATED DEFS //
+		const calculateDefs = () => {
+
+
+
+
+
+
+
+			return data;
+		};
+
+		populateFakeData();
+		return calculateDefs();
+	};
+		
+
+
+
+
+	////////////////////////////////////////////////////////////////
+		// RenderWidget Func
+	////////////////////////////////////////////////////////////////
+
+	const renderWidget = (widget, data) => {
+		// ********************************************* DRAW ******************************************************* //
+		widget.outerDiv 
+			.style('height', data.jqHeight + 'px')	//only for browser
+			.style('width', data.jqWidth + 'px')		//only for browser
+
+		widget.svg 
+			.attr('height', data.jqHeight + 'px')
+			.attr('width', data.jqWidth + 'px')
+			
+		d3.select(widget.svg.node().parentNode).style('background-color', data.backgroundColor);
+		
+		// delete leftover elements from versions previously rendered
+		if (!widget.svg.empty()) resetElements(widget.svg, '*');
+
+		// ********************************************* GRAPHIC GROUP ******************************************************* //
+
+	
+
+		const graphicGroup = widget.svg.append('g').attr('class', 'graphicGroup');
+
+
+		const centeredGroup = graphicGroup.append('g')
+			.attr('class', 'centeredGroup')
+			.attr('transform', `translate(${data.jqWidth / 2}, ${data.jqHeight / 2})`);
+		
+		// ********************************************* OUTER ELLIPSE ******************************************************* //
+		centeredGroup.append('ellipse')
+			.attr('rx', 40)
+			.attr('ry', 20)
+
+	};
+	
+
+
+
+
+
+
+	////////////////////////////////////////////////////////////////
+		// Render Func
+	////////////////////////////////////////////////////////////////
+
+	function render(widget, force) {
+		// invoking setupDefinitions, then returning value to renderWidget func
+		let theData = setupDefinitions(widget);
+		if (force || !widget.data || needToRedrawWidget(widget, theData)){
+			renderWidget(widget, theData);	
+		}
+		widget.data = theData;
+	}
+
+
+
+
+
+
+	////////////////////////////////////////////////////////////////
+		// Initialize Widget
+	////////////////////////////////////////////////////////////////
+	function initialize() {
+		const widget = {};
+
+		widget.outerDiv = d3.select('#outer')
+			.style('overflow', 'hidden');
+		widget.svg = widget.outerDiv.append('svg')
+			.attr('class', 'log')
+			.style('overflow', 'hidden');
+
+		render(widget);
+	}
+
+
+
+
+
+initialize();
+
+}
+
+defineFuncForTabSpacing();
